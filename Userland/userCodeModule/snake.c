d0b2d48daf66b06027920989216e8824992bf016
kndtxt <csilvadiniz@itba.edu.ar>
2023-10-29 20:00:04 -0300

    minor changes

--- Userland/userCodeModule/snake.c
+++ Userland/userCodeModule/snake.c
@@ -2,36 +2,42 @@
 #include "include/drawings.h"
 #include "include/userlib.h"
 
+#define APPLE 1
 #define SNAKE1  7
 #define SNAKE2  14
 
-uint8_t BOARD_H;
-uint8_t BOARD_W;
+uint8_t BOARD_H=20; //basandonos en las dimensiones del de maps
+uint8_t BOARD_W=20;
 
-uint8_t board[1000][1000];//valor grande para inicializarla (evitamos tener que usar malloc)
+uint8_t board[20][20];//valor grande para inicializarla (evitamos tener que usar malloc)
 
 uint8_t fontSize, drawSize;
 uint16_t screenHeight, screenWidth;
 
+uint8_t dibSpaceHeight, dibSpaceWidth;//el valor que realmente toma luego del size
+
+uint8_t board_start_x, board_start_y;
+
 void initGame(){
     getScreenData(&screenHeight,&screenWidth,&fontSize,&drawSize);
-    BOARD_H=screenHeight/dibHeight;
-    BOARD_W=screenWidth/dibWidth;
+    dibSpaceHeight=(dibHeight*drawSize);
+    dibSpaceWidth=(dibWidth*drawSize);
+    BOARD_H= BOARD_H>(screenHeight/dibSpaceHeight)?  (screenHeight/dibSpaceHeight) : BOARD_H;    //buscamos la menor dimension
+    BOARD_W= BOARD_W>(screenWidth/dibSpaceWidth)?  (screenWidth/dibSpaceWidth) : BOARD_W;
+    BOARD_H>BOARD_W? (BOARD_H=BOARD_W) : (BOARD_W=BOARD_H);//nos aseguramos de que el tablero sea cuadrado
     for(int i=0; i<BOARD_W; i++){
         for(int j=0; j<BOARD_H; j++){
             board[i][j]=0;
         }
     }
+    board_start_x=(screenWidth-(BOARD_W*dibSpaceWidth))/2;
+    board_start_y=(screenHeight-(BOARD_H*dibSpaceHeight))/2;
 }
 
 //================================================================================================================================
 // Tablero de juego
 //================================================================================================================================
 //================================================================================================================================
-//logistica: casilleros vacios almacenan 0, las manzanas suman 1.
-//identificamos que en un casillero una snake comio manzana ya que su contador incremento solo en 1
-//identificamos choques entre snakes ya que la suma del casillero va a dar un numero mucho mas alto
-//snake 1= 7;  snake 2= 14;
 void tablero(){
     
     for(uint8_t i=0; i<BOARD_W; i++){
@@ -39,27 +45,11 @@ void tablero(){
             //dibujo la casilla
             uint32_t color= 
                 (( ((i%(2)==0) && (j%(2))) ||  ((i%(2)) && (j%(2)==0))))? BOARDCOLOR1:BOARDCOLOR2;
-            draw(square[0], color, dibHeight, i*(dibWidth*drawSize),j*(dibHeight*drawSize));
+            draw(square[0], color, dibHeight, i*(dibSpaceWidth) + board_start_x, j*(dibSpaceHeight) + board_start_y);
 
             uint8_t casilla = board[i][j];
-            switch(casilla){
-                case(0)://no hay nada
-                    break;
-                case(1)://manzana
-                    draw_manzana(i*(dibWidth*drawSize),j*(dibHeight*drawSize));
-                    break;
-                case(7)://snake1
-                    putSnake(j,i,casilla);
-                    break;
-                case(8)://snake1 comio manzana
-                    break;
-                case(14)://snake2
-                    break;
-                case(15)://snake2 comio manzana
-                    break;
-                default://casos de colision entre serpientes(la logica no es en esta parte, no deberia ocurrir)
-                    break;
-            }
+            if(casilla==APPLE) draw_manzana(i*(dibSpaceWidth) + board_start_x ,j*(dibSpaceHeight) + board_start_y);
+            else if(casilla==SNAKE1 || casilla==SNAKE2) putSnake(j,i,casilla);
         }
     }
 }
@@ -79,34 +69,34 @@ void putSnake(uint8_t row, uint8_t column, uint8_t snake){//contemplamos casos d
     caso += checkUp(row,column,snake)<<3;
     switch(caso){
         case(1):
-            draw_snakehead_left(column*(dibWidth*drawSize),row*(dibHeight*drawSize));
+            draw_snakehead_left(column*(dibSpaceWidth) + board_start_x ,row*(dibSpaceHeight) + board_start_y);
             break;
         case(2):
-            draw_snakehead_right(column*(dibWidth*drawSize),row*(dibHeight*drawSize));
+            draw_snakehead_right(column*(dibSpaceWidth) + board_start_x ,row*(dibSpaceHeight) + board_start_y);
             break;
         case(3):
-            draw_snakebody_horizontal(column*(dibWidth*drawSize),row*(dibHeight*drawSize));
+            draw_snakebody_horizontal(column*(dibSpaceWidth) + board_start_x ,row*(dibSpaceHeight) + board_start_y);
             break;
         case(4):
-            draw_snakehead_up(column*(dibWidth*drawSize),row*(dibHeight*drawSize));
+            draw_snakehead_up(column*(dibSpaceWidth) + board_start_x ,row*(dibSpaceHeight) + board_start_y);
             break;
         case(5):
-            draw_snakecurve_downleft(column*(dibWidth*drawSize),row*(dibHeight*drawSize));
+            draw_snakecurve_downleft(column*(dibSpaceWidth) + board_start_x ,row*(dibSpaceHeight) + board_start_y);
             break;
         case(6):
-            draw_snakecurve_downright(column*(dibWidth*drawSize),row*(dibHeight*drawSize));
+            draw_snakecurve_downright(column*(dibSpaceWidth) + board_start_x ,row*(dibSpaceHeight) + board_start_y);
             break;
         case(8):
-            draw_snakehead_down(column*(dibWidth*drawSize),row*(dibHeight*drawSize));
+            draw_snakehead_down(column*(dibSpaceWidth) + board_start_x ,row*(dibSpaceHeight) + board_start_y);
             break;
         case(9):
-            draw_snakecurve_upleft(column*(dibWidth*drawSize),row*(dibHeight*drawSize));
+            draw_snakecurve_upleft(column*(dibSpaceWidth) + board_start_x ,row*(dibSpaceHeight) + board_start_y);
             break;
         case(10):
-            draw_snakecurve_upright(column*(dibWidth*drawSize),row*(dibHeight*drawSize));
+            draw_snakecurve_upright(column*(dibSpaceWidth) + board_start_x ,row*(dibSpaceHeight) + board_start_y);
             break;
         case(12):
-            draw_snakebody_vertical(column*(dibWidth*drawSize),row*(dibHeight*drawSize));
+            draw_snakebody_vertical(column*(dibSpaceWidth) + board_start_x ,row*(dibSpaceHeight) + board_start_y);
             break;
         default://casos sin sentido
             break;
