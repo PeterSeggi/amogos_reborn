bef149ac5c940374b10b51609fa39a8393fc16f1
kndtxt <csilvadiniz@itba.edu.ar>
2023-12-26 19:41:34 -0300

    some drawings

--- Userland/userCodeModule/include/tetris_drawings.h
+++ Userland/userCodeModule/include/tetris_drawings.h
@@ -12,4 +12,8 @@
 void draw_tetris_board();
 void set_init_values(uint16_t screenHeight, uint16_t screenWidth, uint8_t squareSpaceHeight, uint8_t squareSpaceWidth);
 
+void draw_tetris_block(uint64_t init_x, uint64_t init_y, uint32_t block_color);
+void draw_block(uint16_t bitmap[][squareHeight], uint16_t layers, uint32_t colors[], uint64_t init_x, uint64_t init_y, uint32_t block_color);
+
+
 #endif
--- Userland/userCodeModule/tetris_drawings.c
+++ Userland/userCodeModule/tetris_drawings.c
@@ -22,6 +22,65 @@ uint16_t squareT[][squareHeight]={
      0b1111111111111111}
 };
 
+#define TETRIS_FIGURE_SQUARE_LAYERS 2
+uint32_t tetris_figure_square_colors[TETRIS_FIGURE_SQUARE_LAYERS]={0x00FBFBFB,0x00000000};
+uint16_t tetrisFigureSquare[][squareHeight]={
+
+//color
+    {0b1111111111111111,
+     0b1111111111111111,
+     0b1111111111111111,
+     0b1111111111111111,
+     0b1111111111111111,
+     0b1111111111111111,
+     0b1111111111111111,
+     0b1111111111111111,
+     0b1111111111111111,
+     0b1111111111111111,
+     0b1111111111111111,
+     0b1111111111111111,
+     0b1111111111111111,
+     0b1111111111111111,
+     0b1111111111111111,
+     0b1111111111111111},
+
+//white
+    {0b1111111111111111,
+     0b1111111111111111,
+     0b1111000000000011,
+     0b1111000000000011,
+     0b1100111100000011,
+     0b1100111100000011,
+     0b1100110000000011,
+     0b1100110000000011,
+     0b1100000000000011,
+     0b1100000000000011,
+     0b1100000000000011,
+     0b1100000000000011,
+     0b1100000000000011,
+     0b1100000000000011,
+     0b1111111111111111,
+     0b1111111111111111},
+
+    //border
+    {0b1111111111111111,
+     0b1111111111111111,
+     0b1100000000000011,
+     0b1100000000000011,
+     0b1100000000000011,
+     0b1100000000000011,
+     0b1100000000000011,
+     0b1100000000000011,
+     0b1100000000000011,
+     0b1100000000000011,
+     0b1100000000000011,
+     0b1100000000000011,
+     0b1100000000000011,
+     0b1100000000000011,
+     0b1111111111111111,
+     0b1111111111111111},
+};
+
 //================================================================================================================================
 //functions
 #define GAMESCREEN_W 10
@@ -51,15 +110,38 @@ void draw_tetris_board(){
         }
     }
 
+
     //gamescreen
     for(int i =0; i<GAMESCREEN_W; i++){
         for(int j=0; j<GAMESCREEN_H;j++){
             draw(squareT[0], 0x00000000, squareHeight,gamescreen_x_init+i*(squareSpaceWidth),gamescreen_y_init + j*(squareSpaceHeight));
         }
     }
+
+    squareSpaceHeight/=2;
+    squareSpaceWidth/=2;
+
+    draw_tetris_block(gamescreen_x_init+squareSpaceWidth*3,gamescreen_y_init+squareSpaceHeight*3,0x00FF0000);
+    draw_tetris_block(gamescreen_x_init+squareSpaceWidth*4,gamescreen_y_init+squareSpaceHeight*3,0x00FF0000);
+    draw_tetris_block(gamescreen_x_init+squareSpaceWidth*3,gamescreen_y_init+squareSpaceHeight*4,0x00FF0000);
+    draw_tetris_block(gamescreen_x_init+squareSpaceWidth*2,gamescreen_y_init+squareSpaceHeight*4,0x00FF0000);
+
 }
 
 void set_init_values(uint16_t screenHeight, uint16_t screenWidth, uint8_t squareSpaceHeight, uint8_t squareSpaceWidth){
     gamescreen_x_init=(((screenWidth/squareSpaceWidth)-GAMESCREEN_W)/2)*squareSpaceWidth;
     gamescreen_y_init=squareSpaceHeight*2;
+}
+
+void draw_tetris_block(uint64_t init_x, uint64_t init_y,uint32_t block_color){
+    draw_block(tetrisFigureSquare, TETRIS_FIGURE_SQUARE_LAYERS, tetris_figure_square_colors,init_x,init_y,block_color);
+}
+
+void draw_block(uint16_t bitmap[][squareHeight], uint16_t layers, uint32_t colors[], uint64_t init_x, uint64_t init_y, uint32_t block_color){
+    changeDrawSize(1);
+    for(int i=0; i<layers+1; i++){
+        if(i==0) draw(bitmap[i], block_color, squareHeight, init_x, init_y);
+        else draw(bitmap[i], colors[i-1], squareHeight, init_x, init_y);
+    }
+    changeDrawSize(2);
 }
