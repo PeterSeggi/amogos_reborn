706fa670ad23413eddb773078d7031ccd92f6d89
kndtxt <csilvadiniz@itba.edu.ar>
2024-05-27 22:16:33 -0300

    polish up real nice!

--- Kernel/mman.c
+++ Kernel/mman.c
@@ -1,14 +1,14 @@
 #include <mman.h>
 
-//asumimos mem libre a partir de 0x600000
-//como es de 512M el qemu, inyectamos el valor 0x20000000
+//assumes free mem from 0x600000
+//qemu flag gives 512M -> 0x20000000
 #define FREE_MEM_START 0x600000
 #define FREE_MEM_END 0x20000000
 
-#define MEM_CHUNK 8 //queremos que de direcciones alineadas
+#define MEM_CHUNK 8 //for alignment
 
 /*
-    WORDMAP: 2Byte mapean 8 byte(mem_chunk)
+    WORDMAP: 2Byte maps 8 byte(mem_chunk)
     
      ________________ ____
     |________________|____|
@@ -22,16 +22,17 @@ uint64_t total_mem = 0;
 uint64_t vacant_mem = 0;
 uint64_t occupied_mem = 0;
 
+
 void * assign_mem(uint16_t size);
 
 void mm_init(){
-    total_mem = (FREE_MEM_END - FREE_MEM_START);//queremos multiplos de mem_chunk
+    total_mem = (FREE_MEM_END - FREE_MEM_START);
     total_mem -= total_mem%MEM_CHUNK;
     total_mem -= (total_mem/MEM_CHUNK)*2;
     vacant_mem = total_mem;
     occupied_mem = 0;
     header = FREE_MEM_START + total_mem;
-    for(int i = 0; (header+i) < FREE_MEM_END; i++){//set 0 de manager mem
+    for(int i = 0; (header+i) < FREE_MEM_END; i++){//fill manager with 0
         *(header+i)=0;
     }
 }
@@ -43,32 +44,6 @@ void * my_malloc(uint16_t size){
 
     if(size && (get_mem_vacant() >= size)){//TODO: race condition (thread safe? in case multiple assign mem?)
         to_ret = (void *) assign_mem(size/MEM_CHUNK);
-    }
-
-    return to_ret;
-}
-
-void * assign_mem(uint16_t size){
-    void *to_ret = NULL;
-
-    uint8_t set = 0;
-    uint64_t idx = 0;
-
-    for(idx = 0; (idx < total_mem/MEM_CHUNK) && (!set);){
-        if(!(*(header+idx))){
-            uint64_t j = 0;
-            while(!(*(header+idx+j)) && j<size && (idx+j) < total_mem/MEM_CHUNK) j++;
-            if(j == size) set = 1;
-            else idx += j;
-        }
-        else idx += *(header+idx);
-    }
-    if(set){
-        *(header + idx) = size;
-        to_ret = FREE_MEM_START + idx*MEM_CHUNK;
-
-        vacant_mem-=size*MEM_CHUNK;
-        occupied_mem+=size*MEM_CHUNK;
     }
 
     return to_ret;
@@ -101,26 +76,38 @@ uint64_t get_mem_occupied(){
 	return occupied_mem;
 }
 
-//TESTING
-//TODO: esto no va en la entrega final!!
-/*
-int main(){
-	mm_init();
-	int *ptr = (int *) my_malloc(sizeof(int));
+/*----------------------
+  | Local functions
+  -----------------------*/
 
-	if(!ptr) exit(1);
-
-	*ptr = 5;
-
-
-	int *ptr2 = (int *) my_malloc(sizeof(int));
-
-	if(!ptr2) exit(1);
-
-	*ptr2 = 7;
-
-	my_free(ptr2);
-
-    my_free(ptr);
-}
+/**
+*@brief     Looks for the first available space in the management structure.
+*@param[in] size Desired amount of "MEM_CHUNK"s to be allocated.
+*@return    Address for the desired size.
+*@note      If a suitable address isn't found, returns null.
 */
+void * assign_mem(uint16_t size){
+    void *to_ret = NULL;
+
+    uint8_t set = 0;
+    uint64_t idx = 0;
+
+    for(idx = 0; (idx < total_mem/MEM_CHUNK) && (!set);){
+        if(!(*(header+idx))){
+            uint64_t j = 0;
+            while(!(*(header+idx+j)) && j<size && (idx+j) < total_mem/MEM_CHUNK) j++;
+            if(j == size) set = 1;
+            else idx += j;
+        }
+        else idx += *(header+idx);
+    }
+    if(set){
+        *(header + idx) = size;
+        to_ret = FREE_MEM_START + idx*MEM_CHUNK;
+
+        vacant_mem-=size*MEM_CHUNK;
+        occupied_mem+=size*MEM_CHUNK;
+    }
+
+    return to_ret;
+}
